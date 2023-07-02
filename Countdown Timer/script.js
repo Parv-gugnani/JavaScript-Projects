2688957993b341a2516cb600aa2627a778c26f22
Parv-gugnani <9765.parv@gmail.com>
2023-07-02 10:36:36 +0530

    countdown

---
+++ Countdown Timer/script.js
@@ -0,0 +1,24 @@
+const targetDate = new Date("July 10, 2023 15:00:00").getTime();
+
+const countdownInterval = setInterval(() => {
+  const now = new Date().getTime();
+
+  const timeDifference = targetDate - now;
+
+  const days = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
+  const hours = Math.floor(
+    (timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+  );
+  const minutes = Math.floor((timeDifference % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((timeDifference % (1000 * 60)) / 1000);
+
+  document.getElementById("days").textContent = days;
+  document.getElementById("hours").textContent = hours;
+  document.getElementById("minutes").textContent = minutes;
+  document.getElementById("seconds").textContent = seconds;
+
+  if (timeDifference < 0) {
+    clearInterval(countdownInterval);
+    document.getElementById("timer").innerHTML = "Countdown expired";
+  }
+}, 1000);
