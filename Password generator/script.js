13a70d8a59f7225aa6628be58c8e7aaa1f3d108a
Parv-gugnani <9765.parv@gmail.com>
2023-07-07 15:43:18 +0530

    completed

---
+++ Password generator/script.js
@@ -0,0 +1,30 @@
+const passwordBox = document.getElementById("password");
+const length = 10;
+
+const upperCase = "ABCDEFGHIJKLMNOPQRST";
+const lowerCase = "abcdefghijklmnopqrstuvwxyz";
+
+const numbers = "0123456789";
+const specialCharacters = "!@#$%^&*()_+";
+
+const allChars = upperCase + lowerCase + numbers + specialCharacters;
+
+function createPassword() {
+  let password = "";
+  password += upperCase[Math.floor(Math.random() * upperCase.length)];
+  password += lowerCase[Math.floor(Math.random() * lowerCase.length)];
+  password += numbers[Math.floor(Math.random() * numbers.length)];
+  password +=
+    specialCharacters[Math.floor(Math.random() * specialCharacters.length)];
+
+  while (length > password.length) {
+    password += allChars[Math.floor(Math.random() * allChars.length)];
+  }
+  passwordBox.value = password;
+}
+
+function copyPassword() {
+  passwordBox.select();
+  document.execCommand("copy");
+  //   alert("Password copied to clipboard");
+}
