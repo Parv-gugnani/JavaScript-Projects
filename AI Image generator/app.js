5836c87d9b955b5ec8db26e3606914949396db5d
Parv-gugnani <9765.parv@gmail.com>
2023-07-11 00:17:04 +0530

    fucking error

--- AI Image generator/app.js
+++ AI Image generator/app.js
@@ -1,4 +1,5 @@
-const API_KEY = "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";
+/*
+const API_KEY = "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";
 const submitIcon = document.querySelector("#submit-icon");
 const inputElement = document.querySelector("input");
 const imagesSection = document.querySelector(".images-section");
@@ -11,8 +12,8 @@ const getImages = async () => {
       "Content-Type": "application/json",
     },
     body: JSON.stringify({
-      prompt: "A cute baby sea otter",
-      n: 2,
+      prompt: inputElement.value,
+      n: 4,
       size: "1024x1024",
     }),
   };
@@ -41,4 +42,67 @@ const getImages = async () => {
   }
 };
 
+submitIcon.addEventListener("click", getImages);
+*/
+
+// Get the required elements from the HTML
+const submitIcon = document.querySelector("#submit-icon");
+const inputElement = document.querySelector("input");
+const imagesSection = document.querySelector(".images-section");
+
+// Function to handle image generation
+const getImages = async () => {
+  const API_KEY = "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";
+
+  // Prepare the API request options
+  const options = {
+    method: "POST",
+    headers: {
+      Authorization: `Bearer ${API_KEY}`,
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      prompt: inputElement.value,
+      n: 4,
+      size: "1024x1024",
+    }),
+  };
+
+  try {
+    const response = await fetch(
+      "https://api.openai.com/v1/images/generations",
+      options
+    );
+
+    // Check if the response was successful (status code 200) before parsing the JSON
+    if (response.ok) {
+      const data = await response.json();
+
+      if (data && data.data) {
+        // Clear previous images from the imagesSection
+        imagesSection.innerHTML = "";
+
+        data.data.forEach((imageObject) => {
+          const imageContainer = document.createElement("div");
+          imageContainer.classList.add("image-container");
+
+          const imageElement = document.createElement("img");
+          imageElement.setAttribute("src", imageObject.url);
+          imageElement.setAttribute("alt", "Generated Image");
+
+          imageContainer.append(imageElement);
+          imagesSection.append(imageContainer);
+        });
+      } else {
+        console.log("No data or invalid response format");
+      }
+    } else {
+      console.log("Error:", response.status, response.statusText);
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+// Add click event listener to the submit icon
 submitIcon.addEventListener("click", getImages);
