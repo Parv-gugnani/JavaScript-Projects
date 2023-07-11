4b72326258b5c08788e799f7e8856c40096bdd76
Parv-gugnani <9765.parv@gmail.com>
2023-07-11 12:34:45 +0530

    shit

---
+++ x - using api/app.js
@@ -0,0 +1,42 @@
+const submitBtn = document.getElementById("submitBtn");
+const promptInput = document.getElementById("promptInput");
+const imagesSection = document.querySelector(".images-section");
+const API_KEY = "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";
+
+//
+submitBtn.addEventListener("click", async () => {
+  const prompt = promptInput.value;
+
+  const response = await fetch("https://api.openai.com/v1/images/generations", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer YOUR_API_KEY`,
+    },
+    body: JSON.stringify({
+      prompt,
+    }),
+  });
+});
+
+
+  const data = await response.json();
+
+  if (data && Array.isArray(data.images)) {
+    imagesSection.innerHTML = "";
+
+    data.images.forEach((imageUrl) => {
+      const imageContainer = document.createElement("div");
+      imageContainer.classList.add("image-container");
+
+      const imageElement = document.createElement("img");
+      imageElement.setAttribute("src", imageUrl);
+      imageElement.setAttribute("alt", "Generated Image");
+
+      imageContainer.appendChild(imageElement);
+      imagesSection.appendChild(imageContainer);
+    });
+  } else {
+    console.log("Invalid API response");
+  }
+});
