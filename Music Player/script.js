f047a87c50d3ff5eade94e38a250148653f8e5c7
Parv-gugnani <9765.parv@gmail.com>
2023-07-17 17:17:04 +0530

    music player odne

--- Music Player/script.js
+++ Music Player/script.js
@@ -1,10 +1,11 @@
-let progress = document.getElementById("progess");
+// JavaScript code
+let progress = document.getElementById("progress");
 let song = document.getElementById("song");
 let ctrlIcon = document.getElementById("ctrlIcon");
 
 song.onloadedmetadata = function () {
   progress.max = song.duration;
-  progress.value = song.currentTime;
+  progress.value = 0; // Set the initial progress bar value to 0 (start position).
 };
 
 function playPause() {
@@ -19,15 +20,27 @@ function playPause() {
   }
 }
 
-if (song.play()) {
-  setInterval(() => {
-    progress.value = song.currentTime;
-  }, 500);
-}
+let progressInterval;
 
-progress.onchange = function () {
-  song.play();
+ctrlIcon.addEventListener("click", playPause);
+
+song.addEventListener("timeupdate", function () {
+  progress.value = song.currentTime;
+});
+
+progress.addEventListener("input", function () {
   song.currentTime = progress.value;
-  ctrlIcon.classList.add("fa-pause");
-  ctrlIcon.classList.remove("fa-play");
-};
+});
+
+song.addEventListener("play", function () {
+  if (!progressInterval) {
+    progressInterval = setInterval(() => {
+      progress.value = song.currentTime;
+    }, 500);
+  }
+});
+
+song.addEventListener("pause", function () {
+  clearInterval(progressInterval);
+  progressInterval = null;
+});
