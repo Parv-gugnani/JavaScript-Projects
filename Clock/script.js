1f1cdf7229fe475407c7a9e2654962dd52409452
Parv-gugnani <9765.parv@gmail.com>
2023-07-24 09:46:38 +0530

    completed

--- Clock/script.js
+++ Clock/script.js
@@ -2,10 +2,19 @@ let hrs = document.getElementById("hrs");
 let min = document.getElementById("min");
 let sec = document.getElementById("sec");
 
-let currentTime = new Date();
+setInterval(() => {
+  let currentTime = new Date();
 
-console.log(
-  currentTime.getHours(),
-  currentTime.getMinutes(),
-  currentTime.getSeconds()
-);
+  // console.log(
+  //   currentTime.getHours(),
+  //   currentTime.getMinutes(),
+  //   currentTime.getSeconds()
+  // );
+
+  hrs.innerHTML =
+    (currentTime.getHours() < 10 ? "0" : "") + currentTime.getHours();
+  min.innerHTML =
+    (currentTime.getMinutes() < 10 ? "0" : "") + currentTime.getMinutes();
+  sec.innerHTML =
+    (currentTime.getSeconds() < 10 ? "0" : "") + currentTime.getSeconds();
+}, 1000);
