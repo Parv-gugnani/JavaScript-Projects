b9847daa6c554c187990eafc0cdca9a2e6210f0b
Parv-gugnani <9765.parv@gmail.com>
2023-07-26 17:35:19 +0530

    click me'

---
+++ Broken Screen/script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const rickrollButton = document.getElementById("rickroll-btn");
+  rickrollButton.addEventListener("click", function () {
+    window.location.href = " https://www.youtube.com/watch?v=dQw4w9WgXcQ";
+  });
+});
