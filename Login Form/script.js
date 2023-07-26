5c33e6b3744637ba79d9a003ae8f00a5d19a910d
Parv-gugnani <9765.parv@gmail.com>
2023-07-26 10:08:06 +0530

    done

--- Login Form/script.js
+++ Login Form/script.js
@@ -1,34 +1,51 @@
-document
-  .getElementById("loginForm")
-  .addEventListener("submit", function (event) {
-    event.preventDefault();
-    const username = document.getElementById("loginUsername").value;
-    const password = document.getElementById("loginPassword").value;
+// document
+//   .getElementById("loginForm")
+//   .addEventListener("submit", function (event) {
+//     event.preventDefault();
+//     const username = document.getElementById("loginUsername").value;
+//     const password = document.getElementById("loginPassword").value;
 
-    if (username === "admin" && password === "password") {
-      alert("Logged in successfully");
-    } else {
-      alert("Login failed");
-    }
-  });
+//     if (username === "admin" && password === "password") {
+//       alert("Logged in successfully");
+//     } else {
+//       alert("Login failed");
+//     }
+//   });
 
-document
-  .getElementById("registrationForm")
-  .addEventListener("submit", function (event) {
-    event.preventDefault();
-    const username = document.getElementById("regUsername").value;
-    const email = document.getElementById("regEmail").value;
-    const password = document.getElementById("regPassword").value;
-    const confirmPassword = document.getElementById("regConfirmPassword").value;
+// document
+//   .getElementById("registrationForm")
+//   .addEventListener("submit", function (event) {
+//     event.preventDefault();
+//     const username = document.getElementById("regUsername").value;
+//     const email = document.getElementById("regEmail").value;
+//     const password = document.getElementById("regPassword").value;
+//     const confirmPassword = document.getElementById("regConfirmPassword").value;
 
-    if (username && email && password && confirmPassword) {
-      if (password === confirmPassword) {
-        alert("Registration successful");
-        document.getElementById("registrationForm").reset();
-      } else {
-        alert("Passwords do not match");
-      }
-    } else {
-      alert("Please fill all required fields");
-    }
-  });
+//     if (username && email && password && confirmPassword) {
+//       if (password === confirmPassword) {
+//         alert("Registration successful");
+//         document.getElementById("registrationForm").reset();
+//       } else {
+//         alert("Passwords do not match");
+//       }
+//     } else {
+//       alert("Please fill all required fields");
+//     }
+//   });
+
+const loginContainer = document.getElementById("loginContainer");
+const registrationContainer = document.getElementById("registrationContainer");
+const loginLink = document.getElementById("loginLink");
+const registerLink = document.getElementById("registerLink");
+
+loginLink.addEventListener("click", function (event) {
+  event.preventDefault();
+  loginContainer.style.display = "block";
+  registrationContainer.style.display = "none";
+});
+
+registerLink.addEventListener("click", function (event) {
+  event.preventDefault();
+  registrationContainer.style.display = "block";
+  loginContainer.style.display = "none";
+});
