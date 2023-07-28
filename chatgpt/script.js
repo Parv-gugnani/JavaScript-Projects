c2e1d956098c6f0a0c3cd94ecb3b28fbb579f17e
Parv-gugnani <9765.parv@gmail.com>
2023-07-28 23:43:57 +0530

    chatgpt

--- chatgpt/script.js
+++ chatgpt/script.js
@@ -12,29 +12,64 @@ function appendMessageToChatDisplay(message) {
   chatDisplay.scrollTop = chatDisplay.scrollHeight;
 }
 
-function sendMessage() {
-  const userInput = document.getElementById("user-input");
-  const message = userInput.value.trim();
+function botTypingIndicator() {
+  const typingIndicator = createBotMessage(
+    '<div class="typing-indicator">Typing...</div>'
+  );
+  appendMessageToChatDisplay(typingIndicator);
+}
 
-  if (message === "") {
-    return;
-  }
+function clearChat() {
+  document.getElementById("chat-display").innerHTML = "";
+}
 
+function handleUserInput(message) {
   const userMessage = createUserMessage(message);
   appendMessageToChatDisplay(userMessage);
-  userInput.value = "";
 
   // Simulate bot response after a short delay
+  botTypingIndicator();
   setTimeout(() => {
-    const botResponse = createBotMessage(
-      "I'm just a simple bot. I can't respond to you :("
-    );
+    let botResponse;
+
+    // Predefined responses for certain user inputs
+    if (message.toLowerCase().includes("hello")) {
+      botResponse = createBotMessage("Hello there!");
+    } else if (message.toLowerCase().includes("how are you")) {
+      botResponse = createBotMessage(
+        "I'm just a bot, so I don't have feelings, but thanks for asking!"
+      );
+    } else if (message.toLowerCase().includes("clear")) {
+      clearChat();
+      return;
+    } else {
+      botResponse = createBotMessage(
+        "I'm just a simple bot. I can't respond to you :("
+      );
+    }
+
     appendMessageToChatDisplay(botResponse);
-  }, 500);
+  }, 1000);
 }
 
 document.getElementById("user-input").addEventListener("keyup", (event) => {
   if (event.key === "Enter") {
-    sendMessage();
+    const userInput = document.getElementById("user-input");
+    const message = userInput.value.trim();
+
+    if (message !== "") {
+      handleUserInput(message);
+      userInput.value = "";
+    }
+  }
+});
+
+document.getElementById("send-button").addEventListener("click", () => {
+  const userInput = document.getElementById("user-input");
+  const message = userInput.value.trim();
+
+  if (message !== "") {
+    handleUserInput(message);
+    userInput.value = "";
   }
 });
