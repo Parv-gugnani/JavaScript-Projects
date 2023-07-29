d3fc2bdb05f04672c320564ae3bf80cf568a0858
Parv-gugnani <9765.parv@gmail.com>
2023-07-29 16:42:17 +0530

    notification

---
+++ Pop up/script.js
@@ -0,0 +1,9 @@
+function showNotification() {
+  var notificationContainer = document.getElementById("notification-container");
+  notificationContainer.style.display = "flex";
+}
+
+function hideNotification() {
+  var notificationContainer = document.getElementById("notification-container");
+  notificationContainer.style.display = "none";
+}
