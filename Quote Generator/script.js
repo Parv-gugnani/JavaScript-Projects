5e1cd7b49405295200b59a7aea66de713d5af29b
Parv-gugnani <9765.parv@gmail.com>
2023-08-02 09:39:53 +0530

    need to be fixed!

--- Quote Generator/script.js
+++ Quote Generator/script.js
@@ -2,11 +2,22 @@ const api_url = "https://api.quotable.io/random";
 const quote = document.getElementById("quote");
 const author = document.getElementById("author");
 
-async function getquote(url) {
-  const response = await fetch(url);
-  var data = await response.json();
-  quote.innerHTML = data.content;
-  author.innerHTML = data.content;
+async function getQuote(url) {
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    quote.innerHTML = data.content;
+    author.innerHTML = `- ${data.author}`; // Set author name
+  } catch (error) {
+    console.error("Error fetching quote:", error);
+    quote.innerHTML = "An error occurred while fetching the quote.";
+    author.innerHTML = "";
+  }
 }
 
-getquote(api_url);
+getQuote(api_url);
+
+const newQuoteButton = document.getElementById("newQuoteButton");
+newQuoteButton.addEventListener("click", () => {
+  getQuote(api_url);
+});
