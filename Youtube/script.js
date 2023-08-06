ae4717be45ec65ef565bee5e1f39c45a9dac35e9
Parv-gugnani <9765.parv@gmail.com>
2023-08-06 16:36:51 +0530

    youtube clone

---
+++ Youtube/script.js
@@ -0,0 +1,16 @@
+// script.js
+const videoFrame = document.getElementById("videoFrame");
+
+function playVideo() {
+  videoFrame.contentWindow.postMessage(
+    '{"event":"command","func":"playVideo","args":""}',
+    "*"
+  );
+}
+
+function pauseVideo() {
+  videoFrame.contentWindow.postMessage(
+    '{"event":"command","func":"pauseVideo","args":""}',
+    "*"
+  );
+}
