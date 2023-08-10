35332b455e0d64f249cb410204aa5176d3e8daef
Parv-gugnani <9765.parv@gmail.com>
2023-08-10 07:05:14 +0530

    still working!

--- Quora/script.js
+++ Quora/script.js
@@ -18,3 +18,43 @@ function addQuestion(questionText) {
   li.textContent = questionText;
   questionsList.appendChild(li);
 }
+
+// ... Rest of your script ...
+
+// Event listener for answering a question
+function addAnswer(answerText, questionIndex) {
+  const answer = document.createElement("div");
+  answer.className = "answer";
+  answer.innerHTML = `
+    <p>${answerText}</p>
+    <div class="vote-buttons">
+      <button class="vote-button upvote">Upvote</button>
+      <button class="vote-button downvote">Downvote</button>
+    </div>
+  `;
+  const question = questionsList.children[questionIndex];
+  question.appendChild(answer);
+
+  const upvoteButton = answer.querySelector(".upvote");
+  const downvoteButton = answer.querySelector(".downvote");
+  let upvotes = 0;
+  let downvotes = 0;
+
+  upvoteButton.addEventListener("click", () => {
+    upvotes++;
+    updateVoteCount();
+  });
+
+  downvoteButton.addEventListener("click", () => {
+    downvotes++;
+    updateVoteCount();
+  });
+
+  function updateVoteCount() {
+    answer.querySelector(
+      "p"
+    ).textContent = `${answerText} - Upvotes: ${upvotes}, Downvotes: ${downvotes}`;
+  }
+}
+
+// ... Rest of your script ...
