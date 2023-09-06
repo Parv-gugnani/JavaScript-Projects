502e3f06eec2bc90a0eb545d2f7bda21acc97a51
Parv-gugnani <9765.parv@gmail.com>
2023-09-06 09:43:18 +0530

    completed!

--- Music Upload/script.js
+++ Music Upload/script.js
@@ -2,59 +2,62 @@ document.addEventListener("DOMContentLoaded", function () {
   const musicFileInput = document.getElementById("music-file");
   const musicNameInput = document.getElementById("music-name");
   const musicDescriptionInput = document.getElementById("music-description");
-  const uploadButton = document.getElementById("upload-button");
+  const uploadButton = document.getElementById("upload-music-button");
   const musicItemsList = document.getElementById("music-items");
-  const audioPlayer = document.createElement("audio"); // Create audio element
-  const playButton = document.createElement("button"); // Create Play button
-  const pauseButton = document.createElement("button"); // Create Pause button
+  const audioPlayer = document.createElement("audio");
+  const playButton = document.createElement("button");
+  const pauseButton = document.createElement("button");
 
-  // Set initial state for Play and Pause buttons
   playButton.textContent = "Play";
   pauseButton.textContent = "Pause";
   pauseButton.disabled = true;
 
-  // Function to toggle between Play and Pause buttons
   function toggleButtons() {
     playButton.disabled = !playButton.disabled;
     pauseButton.disabled = !pauseButton.disabled;
   }
 
   playButton.addEventListener("click", function () {
-    audioPlayer.src = URL.createObjectURL(musicFileInput.files[0]);
-    audioPlayer.style.display = "block";
-    audioPlayer.play();
-    toggleButtons(); // Show Pause, hide Play
+    if (audioPlayer.src) {
+      audioPlayer.play();
+      toggleButtons();
+    } else {
+      alert("Please select a music file.");
+    }
   });
 
   pauseButton.addEventListener("click", function () {
     audioPlayer.pause();
-    toggleButtons(); // Show Play, hide Pause
+    toggleButtons();
   });
 
   uploadButton.addEventListener("click", function () {
-    const musicFile = musicFileInput.files[0];
     const musicName = musicNameInput.value;
     const musicDescription = musicDescriptionInput.value;
 
-    if (musicFile && musicName && musicDescription) {
-      const listItem = document.createElement("li");
-      listItem.classList.add("music-item");
-
-      const musicInfo = document.createElement("div");
-      musicInfo.innerHTML = `<strong>${musicName}</strong><br>${musicDescription}`;
-
-      listItem.appendChild(playButton);
-      listItem.appendChild(pauseButton);
-      listItem.appendChild(musicInfo);
-      musicItemsList.appendChild(listItem);
-
-      // Reset input fields
-      musicFileInput.value = "";
-      musicNameInput.value = "";
-      musicDescriptionInput.value = "";
-    } else {
-      alert("Please fill in all fields and select a file.");
+    if (musicFileInput.files.length === 0 || !musicName || !musicDescription) {
+      alert("Please fill in all fields and select a music file.");
+      return;
     }
+
+    const listItem = document.createElement("li");
+    listItem.classList.add("music-item");
+
+    const musicInfo = document.createElement("div");
+    musicInfo.innerHTML = `<strong>${musicName}</strong><br>${musicDescription}`;
+
+    listItem.appendChild(playButton);
+    listItem.appendChild(pauseButton);
+    listItem.appendChild(musicInfo);
+    musicItemsList.appendChild(listItem);
+
+    const musicFile = URL.createObjectURL(musicFileInput.files[0]);
+    audioPlayer.src = musicFile;
+
+    musicNameInput.value = "";
+    musicDescriptionInput.value = "";
+    musicFileInput.value = "";
+    toggleButtons();
   });
 
   document.body.appendChild(audioPlayer);
