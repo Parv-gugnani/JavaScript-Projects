28f06e25aaf984be932e92f88fb134c0013db6a9
Parv-gugnani <9765.parv@gmail.com>
2023-09-09 18:48:44 +0530

    completed!

--- Double Click heart/script.js
+++ Double Click heart/script.js
@@ -1,6 +1,15 @@
-const image = document.querySelector(".container");
+const container = document.querySelector(".container");
 const heartIcon = document.querySelector(".heart");
 
-image.addEventListener("dbclick", (e) => {
+container.addEventListener("dblclick", (e) => {
   heartIcon.classList.add("active");
+  let xValue = e.clientX - e.target.offsetLeft;
+  let yValue = e.clientY - e.target.offsetTop;
+
+  heartIcon.style.left = `${xValue}px`;
+  heartIcon.style.top = `${yValue}px`;
+
+  setTimeout(() => {
+    heartIcon.classList.remove("active");
+  }, 1000);
 });
