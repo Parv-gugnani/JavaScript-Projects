2fcb2cadc550f8432aadbe0ab308fbd0c1918ee6
Parv-gugnani <9765.parv@gmail.com>
2023-09-10 20:54:50 +0530

    completed

---
+++ Typing Text/script.js
@@ -0,0 +1,24 @@
+const dynamicText = document.querySelector("h1 span");
+const words = ["Parv", "developer", "Designer", "Billo bagge"];
+let wordIndex = 0;
+let charIndex = 0;
+let isDeleting = false;
+const typeEffect = () => {
+  const currentWord = words[wordIndex];
+  const currentChar = currentWord.substring(0, charIndex);
+  dynamicText.textContent = currentChar;
+  dynamicText.classList.add("stop-blinking");
+  if (!isDeleting && charIndex < currentWord.length) {
+    charIndex++;
+    setTimeout(typeEffect, 200);
+  } else if (isDeleting && charIndex > 0) {
+    charIndex--;
+    setTimeout(typeEffect, 100);
+  } else {
+    isDeleting = !isDeleting;
+    dynamicText.classList.remove("stop-blinking");
+    wordIndex = !isDeleting ? (wordIndex + 1) % words.length : wordIndex;
+    setTimeout(typeEffect, 1200);
+  }
+};
+typeEffect();
