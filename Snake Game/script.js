f34885adb18e1cb901c9afdc47d53de078905977
Parv-gugnani <9765.parv@gmail.com>
2023-09-10 16:41:36 +0530

    completed!

--- Snake Game/script.js
+++ Snake Game/script.js
@@ -1,6 +1,7 @@
 const playBoard = document.querySelector(".play-board");
 const scoreElement = document.querySelector(".score");
 const highScoreElement = document.querySelector(".high-score");
+const controls = document.querySelectorAll(".controls i");
 
 let gameOver = false;
 let foodX, foodY;
@@ -44,6 +45,12 @@ const changeDirection = (e) => {
     velocityY = 0;
   }
 };
+
+controls.forEach((key) => {
+  key.addEventListener("click", () =>
+    changeDirection({ key: key.dataset.key })
+  );
+});
 
 const initGame = () => {
   if (gameOver) return handleGameOver();
