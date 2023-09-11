15a41ee0211e72676df6383218079e9fcdc4ad71
Parv-gugnani <9765.parv@gmail.com>
2023-09-11 17:42:59 +0530

    completed

--- Snake Game/script.js
+++ Snake Game/script.js
@@ -1,3 +1,4 @@
+//Dom
 const playBoard = document.querySelector(".play-board");
 const scoreElement = document.querySelector(".score");
 const highScoreElement = document.querySelector(".high-score");
