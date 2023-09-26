c35b441fd2fa52a03a76e7897e5f424d678bcaf9
Parv-gugnani <9765.parv@gmail.com>
2023-09-26 22:04:24 +0530

    na

--- Rock Paper Scissors/script.js
+++ Rock Paper Scissors/script.js
@@ -1,15 +1,24 @@
-const gameContainer = document.querySelector(".container");
+// Get DOM elements
 const userResult = document.querySelector(".user_result img");
 const botResult = document.querySelector(".bot_result img");
 const result = document.querySelector(".result");
 const optionImages = document.querySelectorAll(".option_image");
 
+// Mapping for user choices
+const userChoices = {
+  rock: "Rock",
+  paper: "Paper",
+  scissors: "Scissors",
+};
+
+// Function to get a random choice for the bot
 function getBotChoice() {
-  const choices = ["rock", "paper", "scissors"];
-  const randomIndex = Math.floor(Math.random() * 3);
+  const choices = Object.keys(userChoices);
+  const randomIndex = Math.floor(Math.random() * choices.length);
   return choices[randomIndex];
 }
 
+// Function to determine the winner
 function determineWinner(userChoice, botChoice) {
   if (userChoice === botChoice) return "Draw";
   if (
@@ -23,15 +32,27 @@ function determineWinner(userChoice, botChoice) {
   }
 }
 
+// Function to update the game results
 function updateGame(userChoice, botChoice) {
-  userResult.src = `${userChoice}.png`;
-  botResult.src = `${botChoice}.png`;
+  userResult.src = `images/${userChoice}.png`;
+  botResult.src = `images/${botChoice}.png`;
 
   const winner = determineWinner(userChoice, botChoice);
 
-  result.textContent = winner === "Draw" ? "It's a Draw!" : `${winner} Wins!`;
+  console.log("User Choice:", userChoice);
+  console.log("Bot Choice:", botChoice);
+  console.log("Winner:", winner);
+
+  if (winner === "Draw") {
+    result.textContent = "It's a Draw!";
+  } else if (winner === "User") {
+    result.textContent = "User Wins!";
+  } else if (winner === "Bot") {
+    result.textContent = "Bot Wins!";
+  }
 }
 
+// Add click event listeners to the option images
 optionImages.forEach((image) => {
   image.addEventListener("click", () => {
     const userChoice = image.getAttribute("data-choice");
