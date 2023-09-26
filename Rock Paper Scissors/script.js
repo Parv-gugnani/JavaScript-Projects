4ac167da788101634dc93052f7d691fb7832f7d2
Parv-gugnani <9765.parv@gmail.com>
2023-09-26 21:41:56 +0530

    style

--- Rock Paper Scissors/script.js
+++ Rock Paper Scissors/script.js
@@ -1,18 +1,15 @@
-// Get DOM elements
 const gameContainer = document.querySelector(".container");
 const userResult = document.querySelector(".user_result img");
 const botResult = document.querySelector(".bot_result img");
 const result = document.querySelector(".result");
 const optionImages = document.querySelectorAll(".option_image");
 
-// Function to get a random choice (rock, paper, or scissors) for the bot
 function getBotChoice() {
   const choices = ["rock", "paper", "scissors"];
   const randomIndex = Math.floor(Math.random() * 3);
   return choices[randomIndex];
 }
 
-// Function to determine the winner
 function determineWinner(userChoice, botChoice) {
   if (userChoice === botChoice) return "Draw";
   if (
@@ -26,7 +23,6 @@ function determineWinner(userChoice, botChoice) {
   }
 }
 
-// Function to update the game results
 function updateGame(userChoice, botChoice) {
   userResult.src = `${userChoice}.png`;
   botResult.src = `${botChoice}.png`;
@@ -36,7 +32,6 @@ function updateGame(userChoice, botChoice) {
   result.textContent = winner === "Draw" ? "It's a Draw!" : `${winner} Wins!`;
 }
 
-// Add click event listeners to the option images
 optionImages.forEach((image) => {
   image.addEventListener("click", () => {
     const userChoice = image.getAttribute("data-choice");
