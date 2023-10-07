a13d5959854922c22a13cc3a921e9bb796119d1a
Parv-gugnani <9765.parv@gmail.com>
2023-10-07 18:16:25 +0530

    audios

--- Piano/script.js
+++ Piano/script.js
@@ -3,4 +3,11 @@ volumeSlider = document.querySelector(".volume-slider input");
 keyCheckbox = document.querySelector(".key-checkbox input");
 
 let allKeys = [];
-audio = new Audio();
+audio = new Audio(`audios/a.wav`);
+
+const playTune = (key) => {
+  audio.src = `tunes/${key}.wav`;
+  audio.play();
+
+  const clickedKey = document.querySelector(`[data-key="${key}"]`);
+};
