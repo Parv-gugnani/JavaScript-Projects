fd78c46e7a43c5df9ac6f01cd4154f2a55eba0be
Parv-gugnani <9765.parv@gmail.com>
2023-10-07 18:11:22 +0530

    styling done

---
+++ Piano/script.js
@@ -0,0 +1,6 @@
+const pianoKeys = document.querySelectorAll(".piano-keys .keygen");
+volumeSlider = document.querySelector(".volume-slider input");
+keyCheckbox = document.querySelector(".key-checkbox input");
+
+let allKeys = [];
+audio = new Audio();
